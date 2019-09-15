396fe485cf93d62440a6cb8d030662ade6dd6701
Álefe Cruz <alefecrz@gmail.com>
2019-09-15 20:04:53 -0300

    Filtrantado Opções do Jogador

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-rl.question('Olá tudo bem?', resposta => {
-    console.log(`Sua resposta: ${resposta}`)
+rl.question('Qual sua escolha Pedra, Papel ou Tesoura?\n', resposta => {
+    if(resposta === 'Tesoura')
+        console.log('Você escolheu ' + resposta)
+    else if(resposta === 'Pedra')
+        console.log('Você escolheu ' + resposta)
+    else if(resposta === 'Papel')
+        console.log('Você escolheu ' + resposta)
+    else
+        console.log('Sua resposta foi: ( ' + resposta +  ' ) .Resposta inváida tente Pedra, Papel ou Tesoura.')
 
     rl.close()
 })
